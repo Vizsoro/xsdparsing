c9601c05ee53492fd7b4dce9596cb5d9c4f04deb
Vizsoro <zsolt.vicze@gmail.com>
2016-06-20 17:47:57 +0200

    Commit.

--- src/vzsolt/xsdparsing/Order.java
+++ src/vzsolt/xsdparsing/Order.java
@@ -1,10 +1,69 @@
 package vzsolt.xsdparsing;
+import java.util.List;
+
+import vzsolt.xsdparsing.item;
+
 
 public abstract class Order {
+  private String orderPerson;
+  private String destination;
+  private List<item> orderedItem;
+  private String orderId;
+  
+  
+
+  public String getOrderPerson() {
+    return orderPerson;
+  }
+
+
+
+  public void setOrderPerson(String orderPerson) {
+    this.orderPerson = orderPerson;
+  }
+
+
+
+  public String getDestination() {
+    return destination;
+  }
+
+
+
+  public void setDestination(String destination) {
+    this.destination = destination;
+  }
+
+
+
+  public List<item> getOrderedItem() {
+    return orderedItem;
+  }
+
+
+
+  public void setOrderedItem(List<item> orderedItem) {
+    this.orderedItem = orderedItem;
+  }
+
+
+
+  public String getOrderId() {
+    return orderId;
+  }
+
+
+
+  public void setOrderId(String orderId) {
+    this.orderId = orderId;
+  }
+
+
 
   public Order() {
     // TODO Auto-generated constructor stub
   }
+  
   
 
 }
--- src/vzsolt/xsdparsing/OrderParser.java
+++ src/vzsolt/xsdparsing/OrderParser.java
@@ -3,7 +3,7 @@ package vzsolt.xsdparsing;
 public class OrderParser {
 
   public OrderParser() {
-    // TODO Auto-generated constructor stub
+   
   }
 
 }
