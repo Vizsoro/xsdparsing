28d4cd123ac3a954e7e879c0e45124770ebf5a5f
Vizsoro <zsolt.vicze@gmail.com>
2016-06-23 16:58:20 +0200

    First commit on this branch. Start to using DOM instead record.

---
+++ src/vzsolt/xsdparse/DOMParser.java
@@ -0,0 +1,29 @@
+package vzsolt.xsdparse;
+
+import java.util.Set;
+
+public class DOMParser implements DocumentParser {
+
+  public DOMParser() {
+    // TODO Auto-generated constructor stub
+  }
+
+  @Override
+  public Set<Record> getBooks() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public Set<Record> getAudioBooks() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public void parseDocuments() {
+    // TODO Auto-generated method stub
+
+  }
+
+}
